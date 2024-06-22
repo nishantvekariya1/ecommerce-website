f344d07df3843326aa6f9dd2bc8e2d0a3a1de15c
nishantvekariya1 <nishantvekariya623@gmail.com>
2024-06-22 15:53:40 +0530

    implement product-card page

--- ecommerce-frontend/src/pages/Home.tsx
+++ ecommerce-frontend/src/pages/Home.tsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom"
+import ProductCard from "../components/product-card"
 
 const Home = () => {
+  const addToCartHandler = () => {
+  };
+
   return (
     <div className="home">
       <section></section>
@@ -13,7 +17,14 @@ const Home = () => {
       </h1>
 
       <main>
-        
+        <ProductCard 
+          productId={"_id"}
+          name={"Macbook"}
+          price={98000}
+          stock={2}
+          handler={addToCartHandler}
+          photo={"https://www.shutterstock.com/image-illustration/macbook-pro-2023-m2-chip-600nw-2328203513.jpg"} 
+        />
       </main>
     </div>
   )
