4aeb7d6318b62fc79417bad1ec386b2aa3f89838
nishantvekariya1 <nishantvekariya623@gmail.com>
2024-06-23 16:45:45 +0530

    implement coupon functionality and api

--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -6,6 +6,7 @@ import morgan from 'morgan';
 import userRoute from "./routes/user.js";
 import productRoute from "./routes/products.js";
 import orderRoute from "./routes/order.js";
+import paymentRoute from "./routes/payment.js";
 
 import { connectDB } from './utils/features.js';
 import { errorMiddleware } from './middlewares/error.js';
@@ -35,6 +36,8 @@ app.use("/api/v1/user",userRoute);
 app.use("/api/v1/product",productRoute);
 
 app.use("/api/v1/order",orderRoute);
+
+app.use("/api/v1/payment",paymentRoute);
 
 app.use("/uploads",express.static("uploads"))
 app.use(errorMiddleware);
